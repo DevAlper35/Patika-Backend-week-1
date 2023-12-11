321596ebfde642158a3e08f3766ea91dcc768070
Alper <devcalisma@gmail.com>
2023-12-11 12:04:57 +0300

    Manav Kasa Programı

---
+++ ManavKasaProgrami/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+// Bu kodlama kullanıcının girdiği Armut, Elma, Domates, Muz ve Patlıcan ağırlıklarına göre toplam fiyatlandırma yapıyor
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double armut = 2.14, elma =3.67, domates = 1.11, muz = 0.95, patlican = 5;
+
+        System.out.print("Armut Kaç Kilo : ");
+        armut *= input.nextDouble();
+        System.out.print("Elma Kaç Kilo : ");
+        elma *= input.nextDouble();
+        System.out.print("Domates Kaç Kilo : ");
+        domates *= input.nextDouble();
+        System.out.print("Muz Kaç Kilo : ");
+        muz *= input.nextDouble();
+        System.out.print("Patlıcan Kaç Kilo : ");
+        patlican *= input.nextDouble();
+
+        double tutar = armut + elma + domates + muz + patlican;
+        System.out.println("Toplam Tutar : " + tutar);
+
+
+
+    }
+}
