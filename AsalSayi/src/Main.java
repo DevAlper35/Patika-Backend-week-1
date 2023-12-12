d5b2035ec62a0f23d6112142e39289298aa15c54
Alper <devcalisma@gmail.com>
2023-12-12 16:26:11 +0300

    Asal Sayı Kodlaması

---
+++ AsalSayi/src/Main.java
@@ -0,0 +1,23 @@
+// Bu kodlamada 1den 100e kadar sayılardan, asal sayı olanları ekrana yazdırıyor.
+public class Main {
+    public static void main(String[] args) {
+        int x, sum = 0;
+
+        System.out.print("1-100 arası asal sayılar");
+
+        for (int i = 1; i <= 100; i++) {
+            sum = 0;
+            for (int j = 1; j <= i; j++) {
+                x = i % j;
+
+                if (x == 0) {
+                    sum += j;
+                }
+            }
+
+            if ((i + 1) == sum) {
+                System.out.print(", " +i);
+            }
+        }
+    }
+}
