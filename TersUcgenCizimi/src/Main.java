74f6ac9f90fca3a15471a691fe58de688ac890d5
DevAlper35 <152246898+DevAlper35@users.noreply.github.com>
2023-12-13 23:10:24 +0300

    TersUcgenCizimi - Odevi

---
+++ TersUcgenCizimi/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+// Kullanıcıdan alınan basamak sayısına göre "*" kullanılarak ters üçgen çizim kodlaması
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Basamak sayısını giriniz: ");
+        int number = input.nextInt();
+
+        for (int i=number-3; i>=0; i--)
+        {
+            for (int j=0; j<(number - i - 1); j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
